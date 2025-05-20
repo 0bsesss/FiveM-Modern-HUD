7b34e75e3f2c3ec62fceb29a2ad8b58a26ccaf45
Emre <113838619+0bsesss@users.noreply.github.com>
2025-05-20 18:37:39 +0300

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,19 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author '0bsess'
+description 'Sade ve modern fivem hud'
+version '1.5.0'
+
+client_scripts {
+    'client/cl_hud.lua'
+}
+
+ui_page 'html/index.html'
+
+files {
+    'html/index.html',
+    'html/css/style.css',
+    'html/js/script.js',
+    'html/assets/mic.png'
+}
